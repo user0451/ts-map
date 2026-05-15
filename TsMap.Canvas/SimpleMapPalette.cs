7fc6df321c410e0dc8169b1776ece29f71511628
user0451 <andy@spinningrock.net>
2026-05-15 13:27:43 +0100

    Change road color in SimpleMapPalette to orange
    Updated the SimpleMapPalette class to use orange instead of white for the Road and PrefabRoad brushes.

--- TsMap.Canvas/SimpleMapPalette.cs
+++ TsMap.Canvas/SimpleMapPalette.cs
@@ -7,8 +7,8 @@ namespace TsMap.Canvas
         public SimpleMapPalette()
         {
             Background = new SolidBrush(Color.FromArgb(72, 78, 102));
-            Road = Brushes.White;
-            PrefabRoad = Brushes.White;
+            Road = Brushes.Orange;
+            PrefabRoad = Brushes.Orange;
             PrefabLight = new SolidBrush(Color.FromArgb(236, 203, 153));
             PrefabDark = new SolidBrush(Color.FromArgb(225, 163, 56));
             PrefabGreen = new SolidBrush(Color.FromArgb(170, 203, 150)); // TODO: Check if green has a specific z-index
